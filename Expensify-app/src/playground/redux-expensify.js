0ea713bdbdc406d042fff420242568a50dcf84c7
CourtneyBritney <courtney02alexander@gmail.com>
2020-09-22 13:43:07 +0200

    Added Files

--- Expensify-app/src/playground/redux-expensify.js
+++ Expensify-app/src/playground/redux-expensify.js
@@ -17,9 +17,20 @@ const addExpense = ({description = '', note = '', amount = 0, createdAt = 0 } = 
 const removeExpense = () => ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE', 
     id
-})
+});
 
+//EDIT EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
 
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
 
 // Expense Reducer
 
@@ -34,6 +45,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             ];
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id );    
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                };
+            });
         default:
             return state;
     }
@@ -50,6 +72,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
         default:
             return state;
     }
@@ -73,6 +100,10 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 const demoState = {
     expenses: [{
@@ -89,4 +120,3 @@ const demoState = {
         endDate: undefined
     }
 };
-
